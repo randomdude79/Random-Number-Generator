775f1e7a3d5235649639115df1b930c60ec18776
randomdude79 <158773778+randomdude79@users.noreply.github.com>
2024-02-04 18:45:20 -0500

    v1.3.1 B
    Shortened the code

--- main.py
+++ main.py
@@ -15,25 +15,18 @@ def get_int(prompt):
 
 def random_gen():
    import random
+   print()
    number1 = get_int('Choose your first number.\n')
    print()
    number2 = get_int('Choose your second number.\n')
    print()
    if number1 > number2:
       print(random.randint(number2, number1),'is your random number.')
-      print()
    if number1 < number2:
       print(random.randint(number1, number2),'is your random number.')
-      print()
    if number1 == number2:
       print('Your numbers are the same, please try again')
-      print()
       random_gen()
-   if number1 == "Feedback":
-      input('What is your feedback for this project?')
-      print('Thank you for your feedback!')
-      random_gen()
-
+      
 while True:
    random_gen()
-   
