e876b235d89e1bb67648c5d8da42efa5115d039c
randomdude79 <158773778+randomdude79@users.noreply.github.com>
2024-02-04 18:50:57 -0500

    1.3.3
    User-friendly fixes on the "Feedback" function

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 def get_int(prompt):
-   i = input(prompt)
-   while i == "Feedback":
-      feed = input('What is your feedback?\n')
-      try: 
-          with open('feedback.txt', 'a') as feedback: 
-              feedback.write(feed + '\n')
-      except Exception:
-          print('Something went wrong')
-      i = input(prompt)
-   while not i.isnumeric():
-      i = input(f'You have entered "{i}".' + 
-             f' "{i}" is not a positive integer. Please enter a positive integer:\n')
-   return int(i)
+  i = input(prompt)
+  while i == "Feedback":
+    feed = input('What is your feedback?\n')
+    try:
+      with open('feedback.txt', 'a') as feedback:
+        feedback.write(feed + '\n')
+        print('Thank you for your feedback!')
+    except Exception:
+      print('Something went wrong')
+    i = input(prompt)
+  while not i.isnumeric():
+    i = input(
+        f'You have entered "{i}".' +
+        f' "{i}" is not a positive integer. Please enter a positive integer:\n'
+    )
+  return int(i)
+
 
 def random_gen():
-   import random
-   print()
-   number1 = get_int('Choose your first number.\n')
-   print()
-   number2 = get_int('Choose your second number.\n')
-   print()
-   if number1 > number2:
-      print(random.randint(number2, number1),'is your random number.')
-   if number1 < number2:
-      print(random.randint(number1, number2),'is your random number.')
-   if number1 == number2:
-      print('Your numbers are the same, please try again')
-      random_gen()
-      
+  import random
+  print()
+  number1 = get_int('Choose your first number.\n')
+  print()
+  number2 = get_int('Choose your second number.\n')
+  print()
+  if number1 > number2:
+    print(random.randint(number2, number1), 'is your random number.')
+  if number1 < number2:
+    print(random.randint(number1, number2), 'is your random number.')
+  if number1 == number2:
+    print('Your numbers are the same, please try again')
+    random_gen()
+
+
 while True:
-   random_gen()
+  random_gen()
