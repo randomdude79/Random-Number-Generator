7c659e194bfee5c637f89c528bad117962f4aa4c
randomdude79 <158773778+randomdude79@users.noreply.github.com>
2024-02-04 20:08:28 -0500

    v1.3.4
    More user-friendly fixes.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def get_int(prompt):
   i = input(prompt)
   while i == "Feedback":
+    print()
     feed = input('What is your feedback?\n')
     try:
       with open('feedback.txt', 'a') as feedback:
         feedback.write(feed + '\n')
+        print()
         print('Thank you for your feedback!')
     except Exception:
       print('Something went wrong')
@@ -12,26 +14,26 @@ def get_int(prompt):
   while not i.isnumeric():
     i = input(
         f'You have entered "{i}".' +
-        f' "{i}" is not a positive integer. Please enter a positive integer:\n'
-    )
+        f' "{i}" is not a positive integer. Please enter a positive integer:\n')
   return int(i)
-
 
 def random_gen():
   import random
-  print()
+  
   number1 = get_int('Choose your first number.\n')
   print()
   number2 = get_int('Choose your second number.\n')
   print()
   if number1 > number2:
     print(random.randint(number2, number1), 'is your random number.')
+    print()
   if number1 < number2:
     print(random.randint(number1, number2), 'is your random number.')
+    print()
   if number1 == number2:
     print('Your numbers are the same, please try again')
+    print()
     random_gen()
-
 
 while True:
   random_gen()
