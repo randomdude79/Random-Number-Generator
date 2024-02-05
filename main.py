8b5f6718d4d38496aa2ed580fd7e9e68f3b084a4
randomdude79 <158773778+randomdude79@users.noreply.github.com>
2024-02-05 15:35:04 -0500

    v1.4.2
    Added "Exit" function.
    Bug fixes.
    Easter Eggs are back!

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+def exit():
+  import sys
+  sys.exit()
 def is_an_integer(s):
   try:
     int(s)
     return True
   except ValueError:
     return False
-    
+
 def get_int(prompt):
   i = input(prompt)
+  while i == "Exit":
+    exit()
   while i == "Feedback":
     print()
     feed = input('What is your feedback?\n')
     print()
-    email =  input('What is your email?\n')
+    email =  input('What is your email? We will use this only to respond to you.\n')
     try:
       with open('feedback.txt', 'a') as feedback:
         feedback.write(feed + '\n' + 'Email:' + email + '\n' + '\n')
@@ -24,25 +29,31 @@ def get_int(prompt):
   while not is_an_integer(i):
     i = input(
         f'You have entered "{i}".' +
-        f' "{i}" is not a positive integer. Please enter a positive integer:\n')
+        f' "{i}" is not an integer. Please enter an integer:\n')
   return int(i)
 
 def random_gen():
   import random
-  
+
   number1 = get_int('Choose your first number.\n')
   print()
   number2 = get_int('Choose your second number.\n')
   print()
   if number1 > number2:
-    print(random.randint(number2, number1), 'is your random number.')
+    print('Your random number is', random.randint(number2, number1))
     print()
   if number1 < number2:
-    print(random.randint(number1, number2), 'is your random number.')
+    print('Your random number is', random.randint(number1, number2))
     print()
   if number1 == number2:
     print('Your numbers are the same, please try again')
     print()
     random_gen()
+  if number1 == 69 and number2 == 420:
+    print('Did you have to do that?')
+    print()
+  if number1 == 420 and number2 == 69:
+    print('Did you have to do that?')
+    print()
 while True:
   random_gen()
