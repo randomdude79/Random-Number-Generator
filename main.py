09408daab7a0836c7524d1e9d048d85337b1adb5
randomdude79 <158773778+randomdude79@users.noreply.github.com>
2024-02-11 10:44:36 -0500

    v1.0

--- main.py
+++ main.py
@@ -1,69 +1,54 @@
-def exit():
-  import sys
-  sys.exit()
+import sys
+import random
 
-def is_an_integer(s):
-  try:
-    int(s)
-    return True
-  except ValueError:
-    return False
+def exit_program():
+    sys.exit()
 
 def get_int(prompt):
-  i = input(prompt)
-  while i == "-0":
-    print('-0 is not an integer. Please try again.')
-    print()
-    i = input(prompt)
-  while i == "Exit" or i == "exit":
-    exit()
-  while i == "Feedback" or i == "feedback":
-    print()
-    feedback = input('What is your feedback?\n')
-    print()
-    email = input(
-        'What is your email? We will use this only to respond to you.\n')
-    try:
-      with open('feedback.txt', 'a') as feed:
-        feed.write(feedback + '\n' + 'Email:' + email + '\n' + '\n')
-        print()
-        print('Thank you for your feedback! We will take a look at it.')
-        print()
-        exit_input = input('Would you like to exit?\n')
-        if exit_input == 'Yes' or exit_input == 'yes':
-          exit()
-        if exit_input == 'No' or exit_input == 'no':
-          while True:
-            random_gen()
-    except Exception:
-      random_gen()
+    while True:
+        i = input(prompt)
+        if i == "-0":
+            print('-0 is not an integer. Please try again.\n')
+        elif i.lower() == "exit":
+            exit_program()
+        elif i.lower() == "feedback":
+            process_feedback()
+        elif not i.isdigit():
+            print(f'You have entered "{i}". "{i}" is not an integer. Please enter an integer.\n')
+        else:
+            return int(i)
 
-  while not is_an_integer(i):
-    i = input(f'You have entered "{i}".' +
-              f' "{i}" is not an integer. Please enter an integer:\n')
-  return int(i)
+def process_feedback():
+    feedback = input('What is your feedback?\n')
+    email = input('What is your email? We will use this only to respond to you.\n')
+    try:
+        with open('feedback.txt', 'a') as feed:
+            feed.write(f'{feedback}\nEmail: {email}\n\n')
+            print('Thank you for your feedback! We will take a look at it.\n')
+            if input('Would you like to exit? (Yes/No): ').lower() == 'yes':
+                exit_program()
+            else:
+                while True:
+                    random_gen()
+    except Exception:
+        random_gen()
 
 def random_gen():
-  import random
-  print()
-  number1 = get_int('Choose your first number.\n')
-  print()
-  number2 = get_int('Choose your second number.\n')
-  print()
-  if number1 > number2:
-    print('Your random number is', random.randint(number2, number2))
-  if number1 < number2:
-    print('Your random number is', random.randint(number1, number2))
-  if number1 == number2:
-    print('Your numbers are the same, please try again')
-    print()
+    number1 = get_int('Choose your first number:\n')
+    number2 = get_int('Choose your second number:\n')
+
+    if number1 > number2:
+        print('Your random number is', random.randint(number2, number1))
+    elif number1 < number2:
+        print('Your random number is', random.randint(number1, number2))
+    else:
+        print('Your numbers are the same, please try again.\n')
+
+    if (number1, number2) in [(69, 420), (420, 69)]:
+        print('Did you have to do that?\n')
+
+while True:
     random_gen()
-  if number1 == 69 and number2 == 420:
-    print('Did you have to do that?')
-    print()
-  if number1 == 420 and number2 == 69:
-    print('Did you have to do that?')
-    print()
 
 while True:
   random_gen()
