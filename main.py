3f08e57a2e13786fe96552765288e68691bf3350
randomdude79 <158773778+randomdude79@users.noreply.github.com>
2024-03-23 20:13:38 -0400

    BETA v 0.1.4
    Actually got longer, but the main code is growing.

--- main.py
+++ main.py
@@ -1,53 +1,68 @@
 import random
+import statistics
+import os
+import sys
+import time
+
+numbersList = []
+
+
+def clear():
+    os.system('clear')
+
+
+def animate(message):
+    for _ in range(4):
+        for c in '|/-\\':
+            sys.stdout.write(f'{message} {c}\r')
+            sys.stdout.flush()
+            time.sleep(0.1)
+
 
 def exit_program():
-    exit()
+    sys.exit()
+
 
 def get_int(prompt):
     while True:
-        i = input(prompt)
-        if i == "-0":
-            print('-0 is not an integer. Please try again.\n')
-        elif i.lower() == "exit":
-            exit_program()
-        elif i.lower() == "feedback":
-            process_feedback()
-        elif not i.isdigit():
-            print(f'You have entered "{i}". "{i}" is not an integer. Please enter an integer.\n')
-        else:
-            return int(i)
+        try:
+            i = int(input(prompt))
+            return i
+        except ValueError:
+            print("Please enter an integer.\n")
 
-def process_feedback():
-    feedback = input('What is your feedback?\n')
-    email = input('What is your email? We will use this only to respond to you.\n')
-    try:
-        with open('feedback.txt', 'a') as feed:
-            feed.write(f'{feedback}\nEmail: {email}\n\n')
-            print('Thank you for your feedback! We will take a look at it.\n')
-            if input('Would you like to exit? (Yes/No): ').lower() == 'yes':
-                exit_program()
-            else:
-                while True:
-                    random_gen()
-    except Exception:
-        random_gen()
 
 def random_gen():
+    global numbersList
+    numbersList = []
     number1 = get_int('Choose your first number:\n')
     number2 = get_int('Choose your second number:\n')
-
-    if number1 > number2:
-        print('Your random number is', random.randint(number2, number1))
-    elif number1 < number2:
-        print('Your random number is', random.randint(number1, number2))
+    seed_choice = input('Would you like to use a seed? (Yes/No)\n')
+    if seed_choice.lower() == 'yes':
+        seed = get_int('Enter your seed:\n')
+        random.seed(seed)
     else:
-        print('Your numbers are the same, please try again.\n')
+        random.seed()
 
-    if (number1, number2) in [(69, 420), (420, 69)]:
-        print('Did you have to do that?\n')
+    amount = get_int('How many numbers do you want to generate?\n')
+    print('Generating your numbers...')
+    time.sleep(1)
+
+    start, end = min(number1, number2), max(number1, number2)
+    random_numbers = [random.randint(start, end) for _ in range(amount)]
+    numbersList.extend(random_numbers)
+
+    print('Your random numbers are:')
+    for num in random_numbers:
+        print(num)
+
+    avg_choice = input('Would you like to find the average of all your numbers? (Yes/No)\n')
+    if avg_choice.lower() == 'yes':
+        print('\nThe average of all your numbers is:', statistics.mean(numbersList), '\n')
+
+    print()
+    return
+
 
 while True:
     random_gen()
-
-while True:
-  random_gen()
